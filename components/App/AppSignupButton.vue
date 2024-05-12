a9d276454151f264e6b1c4af8649af4af34496f2
kotokotodonabe <ksk.pleiades@gmail.com>
2024-05-12 18:55:55 +0900

    Edit Home Toolbar & App Menu Component

---
+++ components/App/AppSignupButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn
+    outlined
+    class="ml-2 font-weight-bold"
+    color="appblue"
+    to="/login"
+  >
+    新規登録
+  </v-btn>
+</template>
+
+<script>
+export default {
+}
+</script>
--- components/Home/HomeAppBar.vue
+++ components/Home/HomeAppBar.vue
@@ -7,24 +7,58 @@
     :elevation="toolbarStyle.elevation"
   >
     <app-logo @click.native="$vuetify.goTo('#scroll-top')" />
-    <v-toolbar-title>
+
+    <v-toolbar-title class="hidden-mobile-and-down">
       {{ appName }}
     </v-toolbar-title>
 
     <v-spacer />
 
-    <v-toolbar-items class="ml-2">
+    <v-toolbar-items
+      class="ml-2 hidden-ipad-and-down"
+    >
       <v-btn
         v-for="(menu, i) in menus"
         :key="`menu-btn-${i}`"
         text
-        @click="$vuetify.goTo(`#${menu.title}`)"
+        :class="{ 'hidden-sm-and-down': (menu.title === 'about') }"
+        @click="goTo(menu.title)"
       >
         {{ menu.title }}
       </v-btn>
     </v-toolbar-items>
-    {{ scrollY }}
-    {{ isScrollPoint }}
+
+    <app-signup-button />
+    <app-login-button />
+
+    <!-- ハンバーガーメニュー -->
+    <v-menu bottom
+      nudge-left="110"
+      nudge-width="100"
+    >
+      <template v-slot:activator="{ on }">
+        <v-app-bar-nav-icon
+          class="hidden-ipad-and-up"
+          v-on="on"
+        />
+      </template>
+      <v-list
+        dense
+        class="hidden-ipad-and-up"
+      >
+        <v-list-item
+          v-for="(menu, i) in menus"
+          :key="`menu-list-${i}`"
+          exact
+          @click="$vuetify.goTo(`#${menu.title}`)"
+        >
+          <v-list-item-title>
+            {{ menu.title }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
   </v-app-bar>
 </template>
 
