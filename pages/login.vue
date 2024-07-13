a526d752b6b990c0b97acd7e51d891a5334a19b4
kotokotodonabe <ksk.pleiades@gmail.com>
2024-07-13 16:47:04 +0900

    Add components for toaster

--- pages/login.vue
+++ pages/login.vue
@@ -74,7 +74,8 @@ export default {
     },
     authFailure ({ response }) {
       if (response && response.status === 404) {
-        // TODO トースター出力
+        const msg = 'ユーザーが見つかりません'
+        return this.$store.dispatch('getToast', { msg })
       }
     }
   }
